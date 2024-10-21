64a4878542ee47cf62f3bccf99b18bdaf1d2ec07
lin17 <lin17>
2024-10-21 19:29:00 +0800

    fix sshfs

--- lua/plugins/func/nvim-tree.lua
+++ lua/plugins/func/nvim-tree.lua
@@ -6,6 +6,10 @@ end
 
 require('nvim-tree').setup({
     on_attach = my_on_attach,
+	--auto_reload_on_write = true,
+	--filesystem_watchers = {
+    --	enable = false,  -- Disable filesystem watchers
+  	--},
     -- git支持
     git = {
         enable = false,
