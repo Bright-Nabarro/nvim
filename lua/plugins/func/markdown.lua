c29ae417fd1e5a6aee5d6bc039cc1278a8a15745
lin17 <bright.nabarrosb@gmail.com>
2025-04-27 10:50:24 +0800

    修复ssh远程访问markdown preview问题，需要手动设置ssh地址

--- lua/plugins/func/markdown.lua
+++ lua/plugins/func/markdown.lua
@@ -1,7 +1,19 @@
 vim.g.mkdp_auto_start = 0
 vim.g.mkdp_auto_close = 0
---vim.g.mkdp_preview_options = { sync_scroll_type = 'top' }
 vim.g.mkdp_refresh_slow = 0
-vim.g.mkdp_open_ip='0.0.0.0'
-vim.g.mkdp_port=9812
+
+local function is_ssh()
+	return vim.env.SSH_CLIENT or vim.env.SSH_TTY
+end
+if is_ssh() then
+  vim.g.mkdp_open_to_the_world = 1          -- 允许远程访问
+  vim.g.mkdp_open_ip           = "192.168.31.67"
+else
+  vim.g.mkdp_open_to_the_world = 0          -- 只在本机
+  vim.g.mkdp_open_ip           = "127.0.0.1"
+end
+
+vim.g.mkdp_port=8080
+vim.g.mkdp_browser = 'none'
+vim.g.mkdp_echo_preview_url = 1
 vim.g.mkdp_theme = 'light'
