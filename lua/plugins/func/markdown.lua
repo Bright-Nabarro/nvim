e7b8988d2dc86d1c6d884021d6ddd6dcbec1488d
lin17 <lin17>
2025-04-27 09:51:21 +0800

    markdown preview to ssh

--- lua/plugins/func/markdown.lua
+++ lua/plugins/func/markdown.lua
@@ -2,3 +2,6 @@ vim.g.mkdp_auto_start = 0
 vim.g.mkdp_auto_close = 0
 --vim.g.mkdp_preview_options = { sync_scroll_type = 'top' }
 vim.g.mkdp_refresh_slow = 0
+vim.g.mkdp_open_ip='0.0.0.0'
+vim.g.mkdp_port=9812
+vim.g.mkdp_theme = 'light'
