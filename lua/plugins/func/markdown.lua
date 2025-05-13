6f75f39fa52354b0d637662ba4b036498b7c414d
lin17 <bright.nabarrosb@gmail.com>
2025-05-13 13:22:18 +0800

    modify markdown

--- lua/plugins/func/markdown.lua
+++ lua/plugins/func/markdown.lua
@@ -8,12 +8,12 @@ end
 if is_ssh() then
   vim.g.mkdp_open_to_the_world = 1          -- 允许远程访问
   vim.g.mkdp_open_ip           = "192.168.31.67"
+	vim.g.mkdp_browser = 'none'
 else
   vim.g.mkdp_open_to_the_world = 0          -- 只在本机
   vim.g.mkdp_open_ip           = "127.0.0.1"
 end
 
 vim.g.mkdp_port=8080
-vim.g.mkdp_browser = 'none'
 vim.g.mkdp_echo_preview_url = 1
 vim.g.mkdp_theme = 'light'
